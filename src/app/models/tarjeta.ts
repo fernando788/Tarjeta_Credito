59a3e64f8ca8c95a913204bfd200eb012d7c6066
fernando788 <44073919+fernando788@users.noreply.github.com>
2023-05-04 10:55:26 -0300

    Version 1

---
+++ src/app/models/tarjeta.ts
@@ -0,0 +1,18 @@
+export class TarjetaCredito{
+    id?: string;
+    titular: string;
+    numeroDeTarjeta: string;
+    cvv: number;
+    fechaCreacion: Date;
+    fechaActualizacion: Date;
+
+    constructor(id: string, titular:string, numeroDeTarjeta:string, 
+        cvv:number, fechaCreacion: Date, fechaActualizacion: Date)
+        {
+            this.titular = titular;
+            this.numeroDeTarjeta= numeroDeTarjeta;
+            this.cvv= cvv;
+            this.fechaCreacion= new Date();
+            this.fechaActualizacion= new Date();
+        }
+}
